74c486365779154d5d16b518afbce6f7e9787ea0
alexandregaeta95-cloud <alexandregaeta95@gmail.com>
2026-07-27 00:13:19 -0300

    Create ProfileTab.tsx

---
+++ src/components/ProfileTab.tsx
@@ -0,0 +1,4 @@
+export { default } from '../modules/profile/components/ProfileTab';
+export * from '../modules/profile/components/ProfileTab';
+export * from '../modules/profile/types/profile';
+export * from '../modules/profile/utils/profileUtils';
