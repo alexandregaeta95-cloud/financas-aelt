bbbd6c18b893118f3a6ef763c84f277bad474b2d
alexandregaeta95-cloud <alexandregaeta95@gmail.com>
2026-07-27 00:16:13 -0300

    Create index.ts

---
+++ src/modules/android/index.ts
@@ -0,0 +1,13 @@
+export * from './types';
+export * from './logs/androidLogger';
+export * from './notification/bankFilter';
+export * from './notification/notificationListenerService';
+export * from './permissions/permissionManager';
+export * from './receiver/duplicateChecker';
+export * from './bridge/androidBridge';
+export * from './service/backgroundService';
+export * from './ai/sprint4Interfaces';
+export * from './plugins/pixNotificationPlugin';
+export * from './components/AndroidLogsViewer';
+export * from './components/AndroidSettingsView';
+
