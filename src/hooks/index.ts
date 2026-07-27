38b05b4c12c3ad491ad18ab5e43c79dd107c9716
alexandregaeta95-cloud <alexandregaeta95@gmail.com>
2026-07-27 00:13:58 -0300

    Create index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export * from '../modules/financeiro/hooks';
+export * from '../modules/veiculos/hooks';
+export * from '../modules/agenda/hooks';
+export * from '../modules/risco/hooks';
+export * from '../modules/pix/hooks';
