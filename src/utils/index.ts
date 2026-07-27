a18567e9b7da369e1a8acb3e724a8644ca6ca070
alexandregaeta95-cloud <alexandregaeta95@gmail.com>
2026-07-27 00:25:24 -0300

    Create index.ts

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export * from '../modules/financeiro/utils';
+export * from '../modules/veiculos/utils';
+export * from '../modules/agenda/utils';
+export * from '../modules/risco/utils';
+export * from '../modules/pix/utils';
