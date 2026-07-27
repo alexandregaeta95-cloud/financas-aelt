e230c1d74ec8300d11efa812ba218f4720e9fe05
alexandregaeta95-cloud <alexandregaeta95@gmail.com>
2026-07-27 00:21:44 -0300

    Create index.ts

---
+++ src/modules/pix/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export * from './services';
+export * from './hooks';
+export * from './utils';
+export * from './dialogs';
+export * from './history';
+export * from './components';
