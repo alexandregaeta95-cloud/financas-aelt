4eabf43089c99d73da1e301921d3e23f4c7a8ca7
alexandregaeta95-cloud <alexandregaeta95@gmail.com>
2026-07-27 00:20:26 -0300

    Create index.ts

---
+++ src/modules/financeiro/components/index.ts
@@ -0,0 +1,3 @@
+export { default as TransactionsTab } from './TransactionsTab';
+export { default as Dashboard } from './Dashboard';
+export { default as TransactionAuditLog } from './TransactionAuditLog';
