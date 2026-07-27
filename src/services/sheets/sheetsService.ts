9d2113af3c28231834e787f75e8eaeaf68d3cce5
alexandregaeta95-cloud <alexandregaeta95@gmail.com>
2026-07-27 00:25:13 -0300

    Create sheetsService.ts

---
+++ src/services/sheets/sheetsService.ts
@@ -0,0 +1,64 @@
+import {
+  findOrCreateSpreadsheet,
+  syncDataToSpreadsheet,
+  fetchTransactionsFromSpreadsheet,
+  fetchAllDataFromSpreadsheet,
+  toSafeString
+} from '../../lib/googleAuth';
+
+export class SheetsService {
+  static async obterOuCriarPlanilha(token?: any): Promise<string> {
+    const safeToken = toSafeString(token);
+    return await findOrCreateSpreadsheet(safeToken);
+  }
+
+  static async sincronizarTudo(
+    token: any,
+    spreadsheetId: any,
+    transactions: any[],
+    infractions: any[] = [],
+    riskZones: any[] = [],
+    medicalAppointments: any[] = [],
+    medicalPrescriptions: any[] = [],
+    compromissos: any[] = [],
+    registeredVehicles: any[] = [],
+    carServicesPerformed: any[] = [],
+    carServicesScheduled: any[] = [],
+    bankAccounts: any[] = [],
+    creditCards: any[] = [],
+    categoryBudgets: { [category: string]: number } = {},
+    customCategories: string[] = [],
+    groceryItems: any[] = []
+  ) {
+    const safeToken = toSafeString(token);
+    const safeSheetId = toSafeString(spreadsheetId);
+    return await syncDataToSpreadsheet(
+      safeToken,
+      safeSheetId,
+      transactions,
+      infractions,
+      riskZones,
+      medicalAppointments,
+      medicalPrescriptions,
+      compromissos,
+      registeredVehicles,
+      carServicesPerformed,
+      carServicesScheduled,
+      bankAccounts,
+      creditCards,
+      categoryBudgets,
+      customCategories,
+      groceryItems
+    );
+  }
+
+  static async buscarTransacoes(token: any, spreadsheetId: any): Promise<any[]> {
+    return await fetchTransactionsFromSpreadsheet(toSafeString(token), toSafeString(spreadsheetId));
+  }
+
+  static async buscarTodosDados(token: any, spreadsheetId: any) {
+    return await fetchAllDataFromSpreadsheet(toSafeString(token), toSafeString(spreadsheetId));
+  }
+}
+
+export const sheetsService = SheetsService;
